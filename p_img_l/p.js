ddb93b85bdb3c54aa536bc320ad1a69bf43a4697
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-07-17 15:43:27 +0530

    never

---
+++ p_img_l/p.js
@@ -0,0 +1,12 @@
+
+
+ var b = prompt('antar image password 👀');
+
+if( b == 'tara' || b == 'Tara' || b == 'TaRa'){
+  
+// document.querySelector('div').style.
+  document.body.querySelector('div').style.backgroundColor = 'transparent';
+  
+}else{
+  window.open('https://i.pinimg.com/originals/df/d0/e9/dfd0e966c4c0869e8574d83ce4776261.gif')
+}
